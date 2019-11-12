b4568688a56f1b4b6a5c1e79ed76166997e97367
pachenya <57573246+pachenya@users.noreply.github.com>
2019-11-12 22:34:22 +0900

    Add files via upload

---
+++ rn.cpp
@@ -0,0 +1,80 @@
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <time.h>
+
+std::string sfureiba[] = {
+  "White",
+  "Black",
+  "Blue",
+  "Red",
+  "Crimson",
+  "Green",
+  "Yello",
+  "Tea-colored",
+  "Shining",
+  "Gray",
+  "Mighty",
+  "Invisible",
+  "Dark",
+  "Happy",
+  "Treat-an-ice",
+  "Small",
+  "Little",
+  "Big",
+  "Fxn'shit",
+  "Lite-Gteen",
+  "Perfect",
+  "Sexy",
+  "Creative",
+  "Great",
+  "Super",
+  "Undead",
+  "Weekly",
+  "Killer",
+  "Fine",
+	"Flying",
+  "Cute",
+	"Friendry",
+	0,
+};
+std::string sone[] = {
+  "Man",
+  "Woman",
+  "Lady",
+  "Girl",
+  "Bob",
+  "Tom",
+  "Marco",
+  "Tom-Marco",
+  "Mio",
+  "Yui",
+  "Ritsu",
+  "Tsumugi",
+  "Azusa",
+  "Sawaco",
+  "Gorio",
+  "Yukari",
+  "Kazuma",
+	"Ogre",
+	"Warrior",
+	"Mage",
+	"Priest",
+	"Rogue",
+	"Fighter",
+	"Writer",
+	"Dog",
+	"Cat",
+	0,
+};
+int main()
+{
+				srand(time(0));
+				return 0;
+}
+
+
+
+
