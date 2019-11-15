80249ec8b2a3a6a6da50ddbf2f0aa7e5b8d27f86
pachenya <57573246+pachenya@users.noreply.github.com>
2019-11-15 19:07:56 +0900

    Add files via upload
    Added some text.

--- rn.cpp
+++ rn.cpp
@@ -35,11 +35,12 @@ std::string sfureiba[] = {
   "Weekly",
   "Killer",
   "Fine",
-	"Flying",
+  "Flying",
   "Cute",
-	"Friendry",
-	"",
+  "Friendry",
+  "",
 };
+
 std::string sone[] = {
   "Man",
   "Woman",
@@ -58,28 +59,50 @@ std::string sone[] = {
   "Gorio",
   "Yukari",
   "Kazuma",
-	"Ogre",
-	"Warrior",
-	"Mage",
-	"Priest",
-	"Rogue",
-	"Fighter",
-	"Writer",
-	"Dog",
-	"Cat",
-	"We",
-	"You",
-	"Me",
-	"They",
-	"Tiger",
-	"Ao-Oni",
-	"Ayumu",
-	"Tomo",
-	"Chiyo",
-	"Sakaki-san",
-	"Warlock",
-	"Elf",
-	"",
+  "Ogre",
+  "Warrior",
+  "Mage",
+  "Priest",
+  "Rogue",
+  "Fighter",
+  "Writer",
+  "Dog",
+  "Cat",
+  "We",
+  "You",
+  "Me",
+  "They",
+  "Tiger",
+  "Ao-Oni",
+  "Ayumu",
+  "Tomo",
+  "Chiyo",
+  "Sakaki-san",
+  "Warlock",
+  "Elf",
+  "Dwarf",
+  "Zombie",
+  "Miki",
+  "Megumi",
+  "Kan-na",
+  "Reimu",
+  "Marisa",
+  "Tenshi",
+  "Alice",
+  "Yohmu",
+  "Reisen",
+  "Air",
+  "Kui",
+  "Degawa",
+  "Surikae",
+  "Dareka",
+  "Spidahma",
+  "Tetsuo",
+  "Jack",
+  "Qween",
+  "King",
+  "Playmate",
+  "",
 };
 
 std::string sdowhat[] = {
@@ -104,7 +127,17 @@ std::string sdowhat[] = {
   "Get",
   "Let it be",
   "Say hello",
-	"",
+  "Talk about self",
+  "Song",
+  "Pacrat",
+  "Expand",
+  "Read the Book",
+  "Open up the door",
+  "Jump",
+  "Bomb",
+  "Spectate",
+  "Break",
+  "",
 };
 
 std::string satw[] = {
@@ -131,44 +164,51 @@ std::string satw[] = {
   "Ice",
   "Bar",
   "Sun",
+  "Underground",
+  "Mars",
+  "Mugi's home",
+  "House made by cakes",
+  "Tsunderera castle",
+  "India",
+  "Dezeni land",
+  "Pinchi",
+  "Westside",
   "",
 };
 
-int get_randint0(int n)
+int
+get_randint0 (int n)
 {
-  if (n==0)
+  if (n == 0)
     return 0;
-	return rand()%n;
+  return rand () % n;
 }
 
-std::string get_rnd_str(std::string s[])
+std::string get_rnd_str (std::string s[])
 {
   std::string rval = "";
-	for(int i=0;s[i].c_str()[0];i++)
-	{
-  	if (get_randint0(i)==0)
-		{
+  for (int i = 0; s[i].c_str ()[0]; i++)
+  {
+    if (get_randint0 (i) == 0)
+    {
       rval = s[i];
-		}
-	}
+    }
+  }
   return rval;
 }
 
-int main()
+int
+main ()
 {
-	srand(time(0));
-	int cnt = 22;
-	for(int i=0;i<cnt;i++)
-	{
-	std::cout << get_rnd_str(sfureiba) << " ";
-  std::cout << get_rnd_str(sone)<< " ";
-	std::cout << get_rnd_str(sdowhat)<< " ";
-	std::cout << "at "<<get_rnd_str(satw)<<"";
-	std::cout << std::endl;
-	}
-				return 0;
+  srand (time (0));
+  int cnt = 22;
+  for (int i = 0; i < cnt; i++)
+  {
+    std::cout << get_rnd_str (sfureiba) << " ";
+    std::cout << get_rnd_str (sone) << " ";
+    std::cout << get_rnd_str (sdowhat) << " ";
+    std::cout << "at " << get_rnd_str (satw) << ".";
+    std::cout << std::endl;
+  }
+  return 0;
 }
-
-
-
-
